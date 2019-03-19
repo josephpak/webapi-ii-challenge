712c32b0b2212d874030add39666b8fb242cbbf8
joepak <josephpak2014@gmail.com>
2019-03-19 16:39:06 -0400

    DELETE and PUT

--- express-router.js
+++ express-router.js
@@ -39,13 +39,57 @@ const router = express.Router();
             if (post) {
                 res.status(200).json(post)
             } else {
-                res.status(404).json({ message: "The post with the specified ID does not exist." 
+                res.status(404).json({ 
+                    message: "The post with the specified ID does not exist." 
                 })
             }     
         } catch (err) {
             res.status(500).json({ 
                 error: "The post information could not be retrieved." 
             })
+        }
+    });
+
+    router.delete('/:id', async (req,res) => {
+        try {
+            const post = await db.findById(req.params.id)
+            if (post) {
+                res.status(200).json({ 
+                    message: "Post successfully deleted" 
+                })
+            } else {
+                res.status(404).json({ 
+                    message: "The post with the specified ID does not exist." 
+                })
+            }     
+        } catch (err) {
+            res.status(500).json({ 
+                error: "The post could not be removed" 
+            })
+        }
+    });
+
+    router.put('/:id', async (req,res) => {
+        try {
+            const post = await db.findById(req.params.id)
+            if (!post) {
+                res.status(404).json({ 
+                    message: "The post with the specified ID does not exist." 
+                })
+            } else if (!req.body.hasOwnProperty('title') || !req.body.hasOwnProperty('contents')) {
+                res.status(400).json ({ 
+                    errorMessage: "Please provide title and contents for the post." 
+                })
+            } else {
+                await db.update(req.params.id, req.body)
+                const newPost = await db.findById(req.params.id)
+                res.status(201).json(newPost);
+            }    
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({
+                error: "The post information could not be modified."
+            });
         }
     })
 
