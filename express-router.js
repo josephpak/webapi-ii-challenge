14aca81160cdc5bf522440999c3fce7ba0e92983
joepak <josephpak2014@gmail.com>
2019-03-19 15:47:29 -0400

    POST and GET all

--- express-router.js
+++ express-router.js
@@ -5,14 +5,20 @@ const db = require('./data/db')
 const router = express.Router();
 
     router.post('/', async (req, res) => {
-        try {
-            const post = await db.insert(req.body);
-            res.status(201).json(post);
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({
-                error: "There was an error while saving the post to the database"
-            });
+        if (!req.body.hasOwnProperty('title') || !req.body.hasOwnProperty('contents')){
+            res.status(400).json ({ 
+                errorMessage: "Please provide title and contents for the post." 
+            })
+        } else {
+            try {
+                const post = await db.insert(req.body);
+                res.status(201).json(post);
+            } catch (err) {
+                console.log(err);
+                res.status(500).json({
+                    error: "There was an error while saving the post to the database"
+                });
+            }
         }
     });
 
